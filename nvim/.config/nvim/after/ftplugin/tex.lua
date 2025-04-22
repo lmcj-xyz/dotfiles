c725720b6d200ddfc94d3239cf1de896ff429b02
LM RGS <pro@lmcj.xyz>
2025-04-22 10:24:16 +0100

    Vimtex imap for \item

--- nvim/.config/nvim/after/ftplugin/tex.lua
+++ nvim/.config/nvim/after/ftplugin/tex.lua
@@ -2,6 +2,7 @@ vim.opt.colorcolumn = "" --remove the line drawn on 80th column for tex files
 
 -- imaps, see :h vimtex-imaps
 vim.fn['vimtex#imaps#add_map']({lhs = '---', rhs = '\\textemdash', wrapper = 'vimtex#imaps#wrap_trivial'})
+vim.fn['vimtex#imaps#add_map']({lhs = '- ', rhs = '\\item', wrapper = 'vimtex#imaps#wrap_trivial'})
 
 vim.snippet.add(
     'bq',
