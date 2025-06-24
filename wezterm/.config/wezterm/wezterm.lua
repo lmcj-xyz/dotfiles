e797c540ed5657fafade2f449137cf0a1a8a8cfa
LM RGS <pro@lmcj.xyz>
2025-06-24 11:36:33 +0100

    Wezterm minimal working config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,18 +4,20 @@ local wezterm = require('wezterm')
 -- This will hold the configuration.
 local config = wezterm.config_builder()
 
+local theme = wezterm.plugin.require('https://github.com/neapsix/wezterm').main
+
 -- This is where you actually apply your config choices
-config.color_scheme = 'Tokyo Night'
+config.font = wezterm.font 'JuliaMono'
+
+config.colors = theme.colors()
+config.window_frame = theme.window_frame()
+
 config.font_size = 16
 config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = 'TITLE | RESIZE'
 
 -- Keybindings
 config.leader = { key = 'q', mods = 'CTRL', timeout_miliseconds = 1000 }
---config.keys = {
---	{ key = '\"', mods = 'LEADER',
---	action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' }, },
---}
 
 -- and finally, return the configuration to wezterm
 return config
