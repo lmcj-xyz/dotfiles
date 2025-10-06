09595350e21adf71bd4736181586d664b500f19a
LM RGS <pro@lmcj.xyz>
2025-10-06 10:00:49 +0100

    Removed org mode stuff

--- nvim/.config/nvim/lua/plugins/orgmode.lua
+++ nvim/.config/nvim/lua/plugins/orgmode.lua
@@ -1,38 +1,39 @@
-local org_directory = '~/Notas'
-return {
-    'chipsenkbeil/org-roam.nvim',
-    tag = '0.1.1',
-    dependencies = {
-        {
-            'nvim-orgmode/orgmode',
-            --tag = '0.3.7',
-            config = function()
-                require('orgmode').setup({
-                    org_agenda_files = org_directory .. '**/*',
-                    org_default_notes_file = org_directory .. 'refile.org',
-                    org_capture_templates = {
-                        -- r = {
-                        --     description = 'Repo',
-                        --     template = "* [[%x][%(return string.match('%x', '([^/]+)$'))]]%?",
-                        --     target = org_directory .. 'repos.org',
-                        -- },
-                    },
-                })
-            end,
-        },
-    },
-    config = function()
-        require('org-roam').setup({
-            directory = org_directory,
-            bindings = {
-                prefix = "<LocalLeader>r",
-            },
-            -- optional
-            --org_files = {
-                --    '~/another_org_dir',
-                --    '~/some/folder/*.org',
-                --    '~/a/single/org_file.org',
-                --}
-            })
-        end
-    }
+-- local org_directory = '~/Notas'
+-- return {
+--     'chipsenkbeil/org-roam.nvim',
+--     tag = '0.1.1',
+--     dependencies = {
+--         {
+--             'nvim-orgmode/orgmode',
+--             --tag = '0.3.7',
+--             config = function()
+--                 require('orgmode').setup({
+--                     org_agenda_files = org_directory .. '**/*',
+--                     org_default_notes_file = org_directory .. 'refile.org',
+--                     org_capture_templates = {
+--                         -- r = {
+--                         --     description = 'Repo',
+--                         --     template = "* [[%x][%(return string.match('%x', '([^/]+)$'))]]%?",
+--                         --     target = org_directory .. 'repos.org',
+--                         -- },
+--                     },
+--                 })
+--             end,
+--         },
+--     },
+--     config = function()
+--         require('org-roam').setup({
+--             directory = org_directory,
+--             bindings = {
+--                 prefix = "<LocalLeader>r",
+--             },
+--             -- optional
+--             --org_files = {
+--                 --    '~/another_org_dir',
+--                 --    '~/some/folder/*.org',
+--                 --    '~/a/single/org_file.org',
+--                 --}
+--             })
+--         end
+--     }
+return {}
