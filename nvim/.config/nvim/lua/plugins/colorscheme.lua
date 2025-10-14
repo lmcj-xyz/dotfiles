098c966ff8e37816351075f8cb717ae60c2e726f
LM RGS <pro@lmcj.xyz>
2025-10-14 12:00:50 +0100

    nvim with kanagawa theme instead of rose-pine

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,16 @@
 return {
+    -- {
+    --     'rose-pine/neovim',
+    --     name = 'rose-pine',
+    --     config = function()
+    --         vim.cmd.colorscheme('rose-pine')
+    --     end
+    -- }
     {
-        'rose-pine/neovim',
-        name = 'rose-pine',
+        'rebelot/kanagawa.nvim',
         config = function()
-            vim.cmd.colorscheme('rose-pine')
+            vim.cmd.colorscheme('kanagawa-dragon')
         end
     }
+
 }
